caff64d1c613efa71aff4fca7b5119aabb498e95
Manny <immanuelmonroe@gmail.com>
2023-03-27 20:41:41 -0400

    Completed exercises 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(str,x) {
+    if (x < 0) {
+        return 'ERROR'
+    }
+    return str.repeat(x)
 };
 
 // Do not edit below this line
