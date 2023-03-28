ea3f7ebf3c1e209355809a324936a7a268849daf
Manny <immanuelmonroe@gmail.com>
2023-03-28 16:36:58 -0400

    Completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  var fTemp = fahrenheit;
+  var fToCel = (fTemp - 32) * 5 / 9;
+  let round = Math.round(fToCel * 10) / 10
+  return round
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  var cTemp = celsius;
+  var cToFahr = cTemp * 9 / 5 + 32;
+  let round = Math.round(cToFahr * 10) / 10
+  return round
 };
 
 // Do not edit below this line
